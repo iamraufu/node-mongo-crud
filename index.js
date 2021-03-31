50b33eaf812ea1fd133b9cf8aeb189cad74448df
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-01 00:37:59 +0600

    server side added

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-// import password from '.env'
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
+const ObjectId = require('mongodb').ObjectId;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
-const password = 'iXYS6zJhx$8PUCp'
+const password = 'iXYS6zJhx$8PUCp';
 
 const uri = "mongodb+srv://organicSoup:iXYS6zJhx$8PUCp@raufuprezensinc.hztjo.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-    res.send('Working!')
+    res.sendFile(__dirname + '/index.html');
 })
 
 client.connect(err => {
@@ -38,24 +33,31 @@ client.connect(err => {
             })
     })
 
-    app.post('/addProduct', (req, res) => {
+    app.post("/addProduct", (req, res) => {
         const product = req.body;
         productCollection.insertOne(product)
             .then(result => {
-                console.log("Success")
-                res.send("Soup Added Successfully!")
+                res.redirect('/')
+            })
+    })
+
+    app.patch('/update/:id', (req, res) => {
+        productCollection.updateOne({ _id: ObjectId(req.params.id) }, {
+                $set: { price: req.body.price, quantity: req.body.quantity }
+            })
+            .then(result => {
+                res.send(result.modifiedCount > 0)
             })
     })
 
     app.delete('/delete/:id', (req, res) => {
         productCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then(result => {
-                console.log(result)
+                res.send(result.deletedCount > 0);
             })
     })
 
-    console.log("Database Connected")
-
 });
 
-app.listen(3000)
+
+app.listen(3000);
